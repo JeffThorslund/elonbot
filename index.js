b506fb2288f73fe8a33d19152ba493c591d9d360
Rohit <61277504+rohitchai@users.noreply.github.com>
2020-08-13 11:30:32 +0200

    Added a fetch tesla's last tweet function '/tesla'.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let bot = new Telegram(token, {
 });
 
 bot.onText(/\/start/, (msg) => {
-    const message = "🚀 ELONBOT\n\nI can help you monitoring Elon's twitter and rapidly notify you.\n\n/monitor - run the bot\n/last - get last tweet\n\nDeveleoped with ❤️ by https://rohit.nl/";
+    const message = "🚀 ELONBOT\n\nI can help you monitoring Elon's twitter and rapidly notify you.\n\n/monitor - run the bot\n/last - get last tweet\n/tesla - get tesla\'s last tweet\n\nDeveleoped with ❤️ by https://rohit.nl/";
     bot.sendMessage(msg.chat.id, message);
 });
 
@@ -62,5 +62,22 @@ bot.onText(/\/monitor/, (msg) => {
 });
 
 bot.onText(/\/help/, (msg) => {
-    bot.sendMessage(msg.chat.id, '💻 Command List:\n\n/monitor - run the elon bot\n/last - get elon\'s last tweet.\n\nHave fun!\n\nDeveleoped with ❤️ by https://rohit.nl/')
+    bot.sendMessage(msg.chat.id, '💻 Command List:\n\n/monitor - run the elon bot\n/last - get elon\'s last tweet\n/tesla - get tesla\'s last tweet\n\nHave fun!\n\nDeveleoped with ❤️ by https://rohit.nl/')
+})
+
+bot.onText(/\/tesla/, (msg) => {
+    params = { screen_name: 'tesla' }
+    client.get('statuses/user_timeline', params, (err, tweets, res) => {
+        if (!err, res.statusCode === 200) {
+            let tweet = tweets[0].text;
+            let created_at = tweets[0].created_at;
+
+            let date = new Date(created_at);
+            let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            let fullDate = `${date.getDate()} ${months[date.getMonth()]} ${date.getUTCHours()}:${date.getUTCMinutes()} UTC`;
+
+            let message = `${tweet}\n\n${fullDate} 🔗`;
+            bot.sendMessage(msg.chat.id, message);
+        }
+    })
 })
