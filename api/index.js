3848ce81261094bed95d24703a5940f0a22eec6e
Rohit <61277504+rohitchai@users.noreply.github.com>
2020-08-13 14:13:45 +0200

    Added REST API file.

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Twitter = require('twitter');
+
+const client = new Twitter({
+    consumer_key: '5T9edPuRipPgC7psKt8CCafv8',
+    consumer_secret: '8yKNDDVUHod8I8aYNtkIPsGJnA4c60dRkyax7vrIql5pk0w7Ye',
+    access_token_key: '1256620928395608066-afgjmJFWXJssU5rDFKLHqNjdFDxsR8',
+    access_token_secret: 'kk9AU6gL5Bu7PXMfhD9UaWds4w8qlGlG42pkhA4HgAg6O'
+});
+
+let params = { screen_name: 'elonmusk' };
+
+const router = express.Router();
+
+router.use('/elonmusk', (req, res) => {
+    client.get('statuses/user_timeline', params, (err, tweets, result) => {
+        if (!err && result.statusCode === 200) {
+            res.send(tweets);
+        }
+    }) 
+})
+
+module.exports = router;
